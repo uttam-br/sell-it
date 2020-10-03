ecded9323adf559e3b3cde5c8a36f03e66159c95
Uttam Rabari <uttamrabari@gmail.com>
2020-10-03 18:11:06 +0530

    fixed forms ui

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 
-import ListingEditScreen from "./app/screens/ListingEditScreen.js";
 import LoginScreen from "./app/screens/LoginScreen";
 
 export default function App() {
